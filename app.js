c8eb4d918d2cda3abb3d4f6e559dd8e9ca5d1464
Wajahat Ahmed <64039031+Wajahat049@users.noreply.github.com>
2020-08-10 00:36:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,62 +4,88 @@ var lst=[["The World Largest desert is ?",["Thar", "Kalahari" ,"Sahara", "Sonora
 ["The chief ore of Aluminium is ?",["Iron", "Cryolite", "Bauxite", "Haematite"]],
 ["The hottest planet in the solar system ?",["Earth","Venus","Mars","Jupiter"]]];
 
+
 var ques = document.getElementsByClassName("ques")
 var opt_1=document.getElementById("opt-1")
 var opt_2=document.getElementById("opt-2")
 var opt_3=document.getElementById("opt-3")
 var opt_4=document.getElementById("opt-4")
-var answers=[3,4,1,3,2]
+var answers=[1,3,4,1,3,2]
 var score=0
 
+// question=""
+// option_1="New York,USA"
+// option_2="Geneva"    
+// option_3="Hague (Netherlands)"
+// option_4="Paris"
 
-function Check(){
-    
-    if(opt_1.checked){
-        option=1
-    }
-    else if(opt_2.checked){
-        option=2
-    }
-    else if(opt_3.checked){
-        option=3
-    }
-    else if(opt_4.checked){
-        option=4
-    }
-}
-console.log(option)
+// document.getElementById("ques").innerText=question
+// document.getElementById("option-1").lastChild.nodeValue=" "+option_1
+// document.getElementById("option-2").lastChild.nodeValue=" "+option_2
+// document.getElementById("option-3").lastChild.nodeValue=" "+option_3
+// document.getElementById("option-4").lastChild.nodeValue=" "+option_4;
+
+
+var optioned=0
 var j=0
-    if(j<4){
-        if(answers[i]===option){
+function Check(){
+    if(opt_1.checked){
+         optioned=1
+    }
+else if(opt_2.checked){
+         optioned=2
+    }
+else if(opt_3.checked){
+         optioned=3
+    }
+else if(opt_4.checked){
+         optioned=4
+    }
+else if((opt_1.checked && opt_2.checked && opt_3.checked && opt_4.checked)==false ){
+        alert("please select a option")
+        
+}
+    if(j<6){
+        if(answers[j]===optioned){
         score+=1;
-        document.getElementById("opt-"+option.toString()).parentNode.style.backgroundColor = "green";
-        document.getElementById("opt-"+option.toString()).parentNode.style.color = "white";
+        document.getElementById("opt-"+optioned.toString()).parentNode.style.backgroundColor = "green";
+        document.getElementById("opt-"+optioned.toString()).parentNode.style.color = "white";
         j++
+        document.getElementById("score").innerHTML="score"+" "+score.toString()+"/6"
+        
         }
         
         else{
-            console.log(document.getElementById("opt-"+option.toString()).parentNode);
-            document.getElementById("opt-"+option.toString()).parentNode.style.backgroundColor = "red";
-            document.getElementById("opt-"+option.toString()).parentNode.style.color = "white";
+            console.log(document.getElementById("opt-"+optioned.toString()).parentNode);
+            document.getElementById("opt-"+optioned.toString()).parentNode.style.backgroundColor = "red";
+            document.getElementById("opt-"+optioned.toString()).parentNode.style.color = "white";
             j++
         }
-        document.getElementById("opt-"+option.toString()).parentNode.style.color = null;
+        document.getElementById("opt-"+answers[i].toString()).disabled=true
+        console.log(document.getElementById("opt-"+answers[j].toString()))
+
+        
         
     }
+
+        
+}
+
 
 
 
 var i=0
 function Next(){ 
-    if(i<5){
+    document.getElementById("opt-"+answers[i].toString()).disabled=false
+    document.getElementById("opt-"+optioned.toString()).parentNode.style.backgroundColor = null;
+    document.getElementById("opt-"+optioned.toString()).parentNode.style.color = null;
+    if(i<6){
 question=lst[i][0]
 option_1=lst[i][1][0]
 option_2=lst[i][1][1]
 option_3=lst[i][1][2]
 option_4=lst[i][1][3]
 document.getElementById("ques").innerText=question
-// console.log(document.getElementById("option-1").lastChild.nodeValue)
 document.getElementById("option-1").lastChild.nodeValue=" "+option_1
 document.getElementById("option-2").lastChild.nodeValue=" "+option_2
 document.getElementById("option-3").lastChild.nodeValue=" "+option_3
